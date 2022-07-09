0bc2a58b113cbf7d4ec1ec6e26f357647d473311
Harshit Rathi <harshitr2001@gmail.com>
2022-07-09 18:10:09 +0545

    updates

--- backend/controllers/projectsController.js
+++ backend/controllers/projectsController.js
@@ -25,15 +25,22 @@ const getSearchResults = catchAsyncErrors(async (req, res) => {
     const { search } = req.query;
     let pattern = new RegExp('^' + search);
     const projects = await Project.find({ name: { $regex: pattern, $options: "i" } })
-              .sort("-createdAt")
-        .populate("developer","name");
+        .sort("-upvotes.length")
+        .sort("-createdAt")
+        .populate("developer", "name")
+        .populate("upvotes", "name")
+        .populate("downvotes", "name");
     const data = await User.find({ name: { $regex: pattern,$options:"i" } });
     res.status(201).json( { data:data, projects } );
 });
 
 const getMyProjects = catchAsyncErrors(async (req, res) => {
     await Project.find({ developer: req.user._id })
-        .populate("developer","name")
+        .sort("-upvotes.length")
+        .sort("-createdAt")
+        .populate("developer", "name")
+        .populate("upvotes", "name")
+        .populate("downvotes","name")
         .then((projects) => {
         res.status(201).json({projects});
     });
@@ -41,7 +48,10 @@ const getMyProjects = catchAsyncErrors(async (req, res) => {
 
 const getProjectById = catchAsyncErrors(async (req, res) => {
     await Project.findById(req.params.id)
-         .populate("developer","name")
+        .populate("developer", "name")
+        .sort("-createdAt")
+        .populate("upvotes", "name")
+        .populate("downvotes","name")
         .then((data) => {
         res.status(201).json(data);
     });
@@ -50,10 +60,12 @@ const getProjectById = catchAsyncErrors(async (req, res) => {
 const getAllProjects = catchAsyncErrors(async (req, res) => {
     await Project.find({})
         .sort("-createdAt")
+        .sort("-upvotes.length")
         .limit(10)
         .populate("developer", "name")
+        .populate("upvotes", "name")
+        .populate("downvotes","name")
         .then((projects) => {
-            console.log(projects);
         res.status(201).json({projects});
     });
 });
@@ -67,20 +79,59 @@ const updateProject = catchAsyncErrors(async (req, res) => {
 });
 
 const deleteProject = catchAsyncErrors(async (req, res) => {
-    await Project.findByIdAndDelete(req.params.id, { new: true }).then((data) => {
+    await Project.findByIdAndDelete(req.params.id, { new: true })
+        .then(() => {
         res.status.json({ message: "Project Deleted" });
     });
 });
 
 const getProjectsByUser = catchAsyncErrors(async (req, res) => {
-    console.log(req.params.id);
-    console.log("gettt");
     await Project.find({ developer: req.params.id })
-              .sort("-createdAt")
-        .populate("developer","name")
+        .sort("-upvotes.length")
+        .sort("-createdAt")
+        .populate("developer", "name")
+        .populate("upvotes", "name")
+        .populate("downvotes", "name")
         .then((projects) => {
-            console.log(projects)
         res.status(201).json({projects});
+    });
+});
+
+
+const upVoteProject = catchAsyncErrors(async (req, res) => {
+    const project = await Project.findById(req.params.id);
+    if (!project.upvotes.includes(req.user._id)) {
+        await Project.findByIdAndUpdate(req.params.id, {
+            $push: {
+                upvotes: req.user._id,
+            },
+            $pull: {                
+                downvotes: req.user._id ,
+            }
+        });
+    }
+    res.status(201).json({
+        success: true,
+        message: "Project Upvoted"
+    });
+});
+
+
+const downVoteProject = catchAsyncErrors(async (req, res) => {
+    const project = await Project.findById(req.params.id);
+    if (project.upvotes.includes(req.user._id)) {
+        await Project.findByIdAndUpdate(req.params.id, {
+        $pull: {
+                upvotes: req.user._id,
+            },
+            $push: {                
+                downvotes: req.user._id ,
+            }
+        });
+    }
+    res.status(201).json({
+        success: true,
+        message: "Project Downvoted"
     });
 });
 
@@ -93,5 +144,7 @@ module.exports = {
     updateProject,
     deleteProject,
     getProjectsByUser,
-    getSearchResults
+    getSearchResults,
+    upVoteProject,
+    downVoteProject
 }
